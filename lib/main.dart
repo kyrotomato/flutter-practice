9540826fbe491fe34f1fb9e39707eaad103fde0d
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-07-20 19:02:26 -0500

    added function to run app and then defined splash page. Added icon and color to splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,35 @@
 import 'package:flutter/material.dart';
+
+//this is the main color. not sure what final is but color maincolor is like a css class
+final Color mainColor = Color(0xFFFF5656);
+
+void main() {
+  //this is where your code lives
+
+  //this is used to run the application
+  //not sure what debugshowcheckedmodebanner is but this sets splashpage to home
+  //splashpage will be defined later
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SplashPage()));
+}
+
+//this is where we define the class splashpage and add content
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //this sets maincolor to the color we set above
+      color: mainColor,
+      //appends a child, stack. stack is multiple widgets stacked on each other
+      child: Stack(
+        children: [
+          Align(
+            //alignment.center will center the children widgets
+            alignment: Alignment.center,
+            //this child is an icon from google icons. Its a vector img
+            child: Icon(Icons.terrain, color: Colors.white, size: 90),
+          )
+        ],
+      ),
+    );
+  }
+}
